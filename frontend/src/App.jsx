721c56d5d245fe10feaa53199463d3c9393b5f32
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-10-02 18:10:24 +0530

    Add TailwindCSS and DaisyUI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+
+      <h1 className=''></h1>
+    </>
+  )
+}
+
+export default App
