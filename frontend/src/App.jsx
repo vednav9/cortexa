1392f59a8049b419796a5616d47e931ea5fcf730
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-10-24 19:52:24 +0530

    Add Routes and Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Hero from './pages/Hero';
-// import Features from './pages/Features';
-// import AIAssistant from './pages/AIAssistant';
-// import Institutions from './pages/Institutions';
-// import Resources from './pages/Resources';
-// import Contact from './pages/Contact';
-// import Login from './pages/Login';
-// import Signup from './pages/Signup';
+import Home from './pages/Home';
+import About from './pages/About';
+import Features from './pages/Features';
+import Reviews from './pages/Reviews';
+import ContactUs from './pages/ContactUs';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -16,14 +15,13 @@ function App() {
       <div className="">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Hero />} />
-          {/* <Route path="/features" element={<Features />} />
-          <Route path="/ai-assistant" element={<AIAssistant />} />
-          <Route path="/institutions" element={<Institutions />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/contact-us" element={<ContactUs />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} /> */}
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </BrowserRouter>
