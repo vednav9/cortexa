f20828fecf6318a3bb19f51258a49f82b6b44b0c
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-12-31 19:20:47 +0530

    created common components for dashboard

---
+++ frontend/src/components/institution/shared/index.js
@@ -0,0 +1,5 @@
+// Shared Components (All Roles)
+export { default as Announcements } from './Announcements';
+export { default as QueryDesk } from './QueryDesk';
+export { default as QAPortal } from './QAPortal';
+export { default as AIChatbot } from './AIChatbot';
