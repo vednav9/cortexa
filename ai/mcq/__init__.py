6e08d0cc7db011ee01fbbd91e176f9fd663a26d9
vednav9 <vedantnavthale222@nhitm.ac.in>
2026-03-13 10:02:57 +0530

    deleting files

--- ai/mcq/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-MCQ Generation Module
-"""
-from .generator import MCQGenerator, get_mcq_generator
-from .validator import MCQValidator
-
-__all__ = ['MCQGenerator', 'get_mcq_generator', 'MCQValidator']
